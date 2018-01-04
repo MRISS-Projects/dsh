2e64ef044f842c3219673064b695f4b772e8215a
Marcelo Riss <marcelo.riss@hp.com>
2018-01-04 02:26:21 -0200

    optimizing imports

--- DSH-rest-api/src/test/java/com/mriss/dsh/restapi/service/DocumentQueueServiceImplTest.java
+++ DSH-rest-api/src/test/java/com/mriss/dsh/restapi/service/DocumentQueueServiceImplTest.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
