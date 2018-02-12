ef9952add2b10da83101fba2748b251acc66110d
riss <riss@riss-HP-ZBook-15-G2>
2018-02-12 01:20:47 -0200

    first site generation with properly configured changes plugin.

--- install-parent-pom.sh
+++ install-parent-pom.sh
@@ -3,7 +3,7 @@
 current_dir=`pwd`
 cp parent-pom.xml /tmp
 mkdir -p /tmp/src/site
-cp ./src/site/site.xml /tmp/src/site
+cp ./src/site-desc/site.xml /tmp/src/site
 cd /tmp
 mvn -Dfile=parent-pom.xml -DpomFile=parent-pom.xml install:install-file
 mvn -f parent-pom.xml site:attach-descriptor
