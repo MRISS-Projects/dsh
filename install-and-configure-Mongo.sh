a881f433d6ff865fbfb157f68e6b3f9c718bec23
Marcelo Riss <marcelo.riss@hp.com>
2019-03-30 18:20:51 -0300

    inserting logs

--- install-and-configure-Mongo.sh
+++ install-and-configure-Mongo.sh
@@ -3,13 +3,12 @@
 #echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 #sudo apt-get update && sudo apt-get install -y mongodb-org
 
+printf "\nsecurity:\n  authorization: enabled\n" >> /etc/mongod.conf
 cat /etc/mongod.conf
 
-printf "\nsecurity:\n authorization: enabled" >> /etc/mongod.conf
-
-cat /etc/mongod.conf
-
+echo "######### before mongo start ##########"
 service mongod start
+echo "######### after mongo start ##########"
 
 mongo --eval "use admin; db.createUser( { user: \"superAdmin\", pwd: \"superAdmin01\", roles: [ { role: \"root\", db: \"admin\" } ] } )"
 mongo -u superAdmin -p superAdmin01 --eval "use dsh; db.createUser( { user: \"dshuser\", pwd: \"dsh01!\", roles: [ \"readWrite\"] } )"
