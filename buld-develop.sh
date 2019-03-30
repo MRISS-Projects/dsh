994ac3edc0aee54fa15bbfeed9b3efd2a1ca4064
Marcelo Riss <marcelo.riss@hp.com>
2019-03-30 00:21:25 -0300

    deploy site

--- buld-develop.sh
+++ buld-develop.sh
@@ -8,7 +8,8 @@ cp maven/settings-security.xml $HOME/.m2
 cp maven/settings-security.xml $HOME/.settings-security.xml
 
 if [ "${TRAVIS_BRANCH}" = "DEVELOP" ]; then
-  echo This is an on-goging development version. Deploying site ...
+  git checkout ${TRAVIS_BRANCH}
+  echo This is an on-goging development version. Just deploying site ...
   mvn -s settings.xml -P deployment clean install && mvn -s settings.xml -P deployment site-deploy
 fi
 rm -rf settings.xml
