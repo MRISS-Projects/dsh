f482332ef636b04bf990dbc09080671c39d0a038
Marcelo Riss <marcelo.riss@hp.com>
2019-03-30 19:15:29 -0300

    trying mongo and rabbitmq services

--- install-and-configure-Mongo.sh
+++ install-and-configure-Mongo.sh
@@ -3,13 +3,7 @@
 #echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 #sudo apt-get update && sudo apt-get install -y mongodb-org
 
-printf "\nsecurity:\n  authorization: enabled\n" >> /etc/mongod.conf
 cat /etc/mongod.conf
-
-echo "######### before mongo start ##########"
-service mongod start
 service mongod status
-echo "######### after mongo start ##########"
-
-mongo --host localhost --port 27017 --eval "use admin; db.createUser( { user: \"superAdmin\", pwd: \"superAdmin01\", roles: [ { role: \"root\", db: \"admin\" } ] } )"
-mongo --host localhost --port 27017 -u superAdmin -p superAdmin01 --eval "use dsh; db.createUser( { user: \"dshuser\", pwd: \"dsh01!\", roles: [ \"readWrite\"] } )"
+mongo --host localhost --eval 'use admin; db.createUser( { user: "superAdmin", pwd: "superAdmin01", roles: [ { role: "root", db: "admin" } ] } )'
+mongo --host localhost -u superAdmin -p superAdmin01 --eval 'use dsh; db.createUser({user:"dshuser",pwd:"dsh01!",roles:[ "readWrite"]})'
