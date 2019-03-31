354cfdbe206d195309ae004a4166fb291fa183b7
Marcelo Riss <marcelo.riss@hp.com>
2019-03-30 23:51:47 -0300

    trying to remove downloading messages

--- build-ci.sh
+++ build-ci.sh
@@ -12,15 +12,15 @@ cp maven/settings-security.xml $HOME/.settings-security.xml
 echo Travis Pull Request: ${TRAVIS_PULL_REQUEST}
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   echo This is a PR. Just testing ...
-  mvn -s settings.xml clean install
+  mvn -B -s settings.xml clean install
 else
   git checkout ${TRAVIS_BRANCH}
   if [ "${TRAVIS_BRANCH}" = "DEVELOP" ]; then
     echo This is an on-goging development version. Just deploying site ...
-    mvn -s settings.xml -P deployment clean install && mvn -s settings.xml -P deployment site-deploy
+    mvn -B -s settings.xml -P deployment clean install && mvn -s settings.xml -P deployment site-deploy
   else
     echo This is a feature branch. Just testing ...
-    mvn -s settings.xml clean install
+    mvn -B -s settings.xml clean install
   fi
 fi
 rm -rf settings.xml
