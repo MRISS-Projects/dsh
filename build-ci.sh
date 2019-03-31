efe1002a28c643c3dc1d46a79af55e6ef80b2a5b
Marcelo Riss <marcelo.riss@hp.com>
2019-03-30 23:55:51 -0300

    trying to disable unnneded downloading messages

--- build-ci.sh
+++ build-ci.sh
@@ -12,15 +12,15 @@ cp maven/settings-security.xml $HOME/.settings-security.xml
 echo Travis Pull Request: ${TRAVIS_PULL_REQUEST}
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   echo This is a PR. Just testing ...
-  mvn -B -s settings.xml clean install
+  mvn -B -s settings.xml -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn clean install
 else
   git checkout ${TRAVIS_BRANCH}
   if [ "${TRAVIS_BRANCH}" = "DEVELOP" ]; then
     echo This is an on-goging development version. Just deploying site ...
-    mvn -B -s settings.xml -P deployment clean install && mvn -s settings.xml -P deployment site-deploy
+    mvn -B -s settings.xml -P deployment -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn clean install && mvn -s settings.xml -P deployment -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn site-deploy
   else
     echo This is a feature branch. Just testing ...
-    mvn -B -s settings.xml clean install
+    mvn -B -s settings.xml -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn clean install
   fi
 fi
 rm -rf settings.xml
