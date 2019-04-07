a0bf4aa5a88e36a6f8ded94fc6cfc4a9c52da9e1
Marcelo Riss <marcelo.riss@hp.com>
2019-04-07 19:42:58 -0300

    testing staging flow

---
+++ build-ci-stage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+chmod 755 *.sh
+
+./install-parent-pom.sh
+
+cp maven/settings-security.xml $HOME/.m2
+cp maven/settings-security.xml $HOME/.settings-security.xml
+
+# start release branch process
+mvn -s settings.xml -DdevelopmentVersion=$NEXT_DEV_VERSION release:branch
