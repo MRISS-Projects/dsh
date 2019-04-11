212099be46155e0c1d8395d9a9d431fd6d12adbf
Marcelo Riss <marcelo.riss@hp.com>
2019-04-11 00:22:19 -0300

    starting release 0.2.0

--- pre-release-script.sh
+++ pre-release-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git fetch --tags origin
+git fetch --tags +refs/heads/*:refs/remotes/origin/*
 git branch --all
 git merge --strategy=recursive --strategy-option=theirs ${BRANCH_TO_RELEASE}
 if [ $? -ne 0 ]
