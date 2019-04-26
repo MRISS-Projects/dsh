76c07a84cf015e33ec02f0a2476f2ed1ac270eb6
Marcelo Riss <marcelo.riss@hp.com>
2019-04-25 21:22:18 -0300

    fixes #63

--- build-ci-staging.sh
+++ build-ci-staging.sh
@@ -11,6 +11,6 @@ cp maven/settings-security.xml $HOME/.settings-security.xml
 
 git checkout ${TRAVIS_BRANCH}
 
-mvn -B -s settings.xml -P deployment,process-badges -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Drelease.type=rcs clean install && mvn -B -s settings.xml -P deployment -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Drelease.type=rcs site-deploy
+mvn -B -s settings.xml -P deployment -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Drelease.type=rcs clean install && mvn -B -s settings.xml -P deployment -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Drelease.type=rcs site-deploy
 
 rm -rf settings.xml
