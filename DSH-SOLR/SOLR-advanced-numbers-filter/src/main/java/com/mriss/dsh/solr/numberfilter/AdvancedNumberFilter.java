8d043919cd186176fa86ba3539ffbf559fddca26
Marcelo Riss <marcelo.riss@animati.com.br>
2020-01-15 22:18:06 -0300

    first version of advanced filter, filtering non-desirable tokens

--- DSH-SOLR/SOLR-advanced-numbers-filter/src/main/java/com/mriss/dsh/solr/numberfilter/AdvancedNumberFilter.java
+++ DSH-SOLR/SOLR-advanced-numbers-filter/src/main/java/com/mriss/dsh/solr/numberfilter/AdvancedNumberFilter.java
@@ -1,80 +1,58 @@
 package com.mriss.dsh.solr.numberfilter;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.FilteringTokenFilter;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
-import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
-import org.apache.lucene.analysis.tokenattributes.TermFrequencyAttribute;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AdvancedNumberFilter extends TokenFilter {
+public class AdvancedNumberFilter extends FilteringTokenFilter {
+
+    public static final String MIXED_NUMBER_1 = "(\\D*)\\-(\\d*)";
+    public static final String MIXED_NUMBER_2 = "(\\d*)\\-(\\D*)";
 
     Logger LOGGER = LoggerFactory.getLogger(AdvancedNumberFilter.class);
 
     private CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
-
-    private TermFrequencyAttribute termFreqAtt = addAttribute(TermFrequencyAttribute.class);
-
-    private OffsetAttribute offsetAtt = addAttribute(OffsetAttribute.class);
-
-    private boolean initialized = false;
-    
-    private int upto = 0;
-
-    private int finalOffset;
-
-    private StringBuffer content = new StringBuffer();
 
     protected AdvancedNumberFilter(TokenStream input) {
         super(input);
     }
 
     @Override
-    public final boolean incrementToken() throws IOException {
-        if (input.incrementToken()) {
-            if (!initialized) {
-                char[] buffer = termAtt.buffer();
-                int length = termAtt.length();
-                content.append(buffer, 0, length).append(' ');
-                upto += length;
-                finalOffset = upto;
-                offsetAtt.setOffset(0, finalOffset);
-            } else {
-                LOGGER.info("Processing term.");
-            }
-            return true;
-        } else {
-            if (!initialized) {
-                initialized = true;
-                LOGGER.info("content: " + content.toString());
-                return true;
-            } else {
-                return false;
-            }
-        }
+    protected boolean accept() throws IOException {
+        int length = termAtt.length();
+        char[] buffer = termAtt.buffer();
+        String contents = new String(buffer, 0, length);
+        return validate(contents);
     }
 
-    public StringBuffer getContent() {
-        return content;
+    private boolean validate(String contents) {
+        return !isUrl(contents) && !isMixedNumber(contents) && !isHostName(contents);
     }
 
-    public void setTermAtt(CharTermAttribute termAtt) {
-        this.termAtt = termAtt;
+    private boolean isHostName(String contents) {
+        Pattern r1 = Pattern.compile(
+                "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])$");
+        Matcher m1 = r1.matcher(contents);
+        return contents.contains(".") && m1.matches();
     }
 
-    @Override
-    public final void end() throws IOException {
-        super.end();
-        // set final offset
-        offsetAtt.setOffset(finalOffset, finalOffset);
+    private boolean isMixedNumber(String contents) {
+        Pattern r1 = Pattern.compile(MIXED_NUMBER_1);
+        Matcher m1 = r1.matcher(contents);
+        Pattern r2 = Pattern.compile(MIXED_NUMBER_2);
+        Matcher m2 = r2.matcher(contents);
+        return m1.matches() || m2.matches();
     }
 
-    @Override
-    public void reset() throws IOException {
-        super.reset();
-        this.initialized = false;
+    private boolean isUrl(String contents) {
+        Pattern r1 = Pattern.compile("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
+        Matcher m1 = r1.matcher(contents);
+        return m1.matches();
     }
 }
--- DSH-SOLR/SOLR-advanced-numbers-filter/src/test/java/com/mriss/dsh/solr/numberfilter/AdvancedNumberFilterTest.java
+++ DSH-SOLR/SOLR-advanced-numbers-filter/src/test/java/com/mriss/dsh/solr/numberfilter/AdvancedNumberFilterTest.java
@@ -1,10 +1,13 @@
 package com.mriss.dsh.solr.numberfilter;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
@@ -13,7 +16,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
@@ -62,17 +64,33 @@ public class AdvancedNumberFilterTest {
     }
 
     @Test
+    public void testRegularExpressionMixedNumbers1() throws Exception {
+        Pattern r1 = Pattern.compile(AdvancedNumberFilter.MIXED_NUMBER_1);
+        Matcher m1 = r1.matcher("canada-39902108");
+        assertTrue(m1.matches());
+        r1 = Pattern.compile(AdvancedNumberFilter.MIXED_NUMBER_2);
+        m1 = r1.matcher("39902108-canada");
+        assertTrue(m1.matches());
+        r1 = Pattern.compile(AdvancedNumberFilter.MIXED_NUMBER_2);
+        m1 = r1.matcher("39902108");
+        assertFalse(m1.matches());
+        r1 = Pattern.compile(AdvancedNumberFilter.MIXED_NUMBER_2);
+        m1 = r1.matcher("canada");
+        assertFalse(m1.matches());
+    }
+
+    @Test
     public void testIncrementToken() throws IOException {
-        Mockito.when(termAtt.buffer()).thenReturn(new char[] { 'a', 'b', 'c' }, new char[] { 'a', 'b', 'c' },
-                new char[] { 'a', 'b', 'c' }, new char[] { 'a', 'b', 'c' }, new char[] { 'a', 'b', 'c' },
-                new char[] { 'a', 'b', 'c' }, new char[] { 'a', 'b', 'c' }, new char[] { 'a', 'b', 'c' });
-        Mockito.when(termAtt.length()).thenReturn(3);
-        filter.setTermAtt(termAtt);
-        boolean a = filter.incrementToken();
-        while (a) {
-            a = filter.incrementToken();
-        }
-        assertEquals("abc abc abc abc ", filter.getContent().toString());
+//        Mockito.when(termAtt.buffer()).thenReturn(new char[] { 'a', 'b', 'c' }, new char[] { 'a', 'b', 'c' },
+//                new char[] { 'a', 'b', 'c' }, new char[] { 'a', 'b', 'c' }, new char[] { 'a', 'b', 'c' },
+//                new char[] { 'a', 'b', 'c' }, new char[] { 'a', 'b', 'c' }, new char[] { 'a', 'b', 'c' });
+//        Mockito.when(termAtt.length()).thenReturn(3);
+//        filter.setTermAtt(termAtt);
+//        boolean a = filter.incrementToken();
+//        while (a) {
+//            a = filter.incrementToken();
+//        }
+//        assertEquals("abc abc abc abc ", filter.getContent().toString());
     }
 
 }
