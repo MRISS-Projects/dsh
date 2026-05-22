967b26a38fcd62fd527be1dfedcd1961bf5c1411
Marcelo Riss <marcelo.riss@gmail.com>
2026-05-21 23:56:26 -0300

    fix: update build scripts and .gitignore for improved deployment configuration

--- post-release-script.sh
+++ post-release-script.sh
@@ -11,8 +11,8 @@ then
     exit 1
 else
     mvn -B -s ../../settings.xml -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Dbasedir=$CURRENT_DIR -Dmessage="release message [skip travis]" scm:checkin
-    mvn -B -s ../../settings.xml -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -P product-release-deployment site-deploy
-    mvn -B -N -s ../../settings.xml -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Dbranch.name=master -P update-readme process-resources
+    mvn -B -s ../../settings.xml -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Dproduct-release-deployment -Ddeployment -Drelease-deployment site-deploy
+    mvn -B -N -s ../../settings.xml -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Dbranch.name=master -Dupdate-readme process-resources
     mvn -B -s ../../settings.xml -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -f dsh-coverage-report/pom.xml -P process-badges process-resources
     #mvn -f dsh-coverage-report/pom.xml -P process-badges process-resources
     #git push origin --delete `echo ${BRANCH_TO_RELEASE} | cut -c 8-`
